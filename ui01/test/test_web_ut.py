b7b34abb36611ceb09328c703fb5ecdd7c19f49a
zsjlu <940142834@qq.com>
2021-01-09 21:44:21 +0800

    iselenium_python 添加C盘路径

--- ui01/test/test_web_ut.py
+++ ui01/test/test_web_ut.py
@@ -14,7 +14,7 @@ class ISelenium(unittest.TestCase):
     # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOMEPATH'], 'iselenium.ini'))
+        config.read('C:'+os.path.join(os.environ['HOMEPATH'], 'iselenium.ini'))
         return config
 
     def tearDown(self):
